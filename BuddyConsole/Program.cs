03827152ce571ce381bb798d0473da02ce031523
Rigo Macias <macias@snazzybadger.com>
2017-09-20 00:33:34 -0500

    checking for testing buddy ci /cd

---
+++ BuddyConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BuddyConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
